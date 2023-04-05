9575364f1349a8569a1f06d6ad10230ca6006e18
Lucas Casal <lucasocteixeira@gmail.com>
2023-04-05 10:40:35 -0300

    virar cartas

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let selectedCard = 0
-function showCard(selectedCard){
-    selectedCard;
-    console.log(selectedCard);
+var card = 0;
 
+function showCard(clicked){
+    card = clicked;
+    card.classList.toggle("flip");
 }
+
+if(showedCards === 2){
+    //conferir as cartas
+    //caso negativo, contar tempo, bloquear a função showCard e virar de volta
+    //caso positivo, manter as cartas
+}
+
