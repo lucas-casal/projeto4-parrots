004a4e6a4b9a56ad915a034730c24e9628ada76c
Lucas Casal <lucasocteixeira@gmail.com>
2023-04-06 00:51:35 -0300

    base desktop pronta

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
 var card = 0;
 var showedCards = 0;
 var paridade = 0;
-const images = ["images/bobrossparrot.gif", "images/explodyparrot.gif", "images/fiestaparrot.gif", "images/metalparrot.gif", "images/revertitparrot.gif", "images/tripletsparrot.gif", "images/unicornparrot.gif"];
+const availableImages = ["images/bobrossparrot.gif", "images/explodyparrot.gif", "images/fiestaparrot.gif", "images/metalparrot.gif", "images/revertitparrot.gif", "images/tripletsparrot.gif", "images/unicornparrot.gif"];
+const availableCards = ["a", "b", "g", "h", "c", "i", "d", "j", "e", "k", "f", "l"];
 
-function begins(){
+
 var cardsNumber = prompt("Você gostaria de jogar com quantas cartas? (escolha um número par de 4 a 12)")
 console.log (cardsNumber % 2)
-function nada(){};
 
 
 while (cardsNumber % 2 === 1 || cardsNumber <  4 || cardsNumber > 14){
     cardsNumber = prompt("Você gostaria de jogar com quantas cartas? (escolha um número par de 4 a 12)")
 }
 
-alert ("let's play!!!")
+alert ("let's play!!!");
+
+
+// criando uma lista aleatória com a quantidade necessária de GIFs
+availableImages.sort(comparador);
+const chosenImages = availableImages.slice(0,cardsNumber / 2);
+function comparador() { 
+	return Math.random() - 0.5; }
+
+    for (x=0; x < cardsNumber / 2; x++) {
+    chosenImages.push(chosenImages[x])
+    console.log(chosenImages);
+    }
+console.log(chosenImages);
+
+// final da lista aleatória de GIFs criada
+
+//criando uma lista aleatória com a quantidade necessária de CARTAS
+const chosenCards = availableCards.slice(0,cardsNumber);
+chosenCards.sort(comparador);
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+console.log(chosenCards);
+//final da lista aleatória de CARTAS
+
+//link GIF com CARTA
+for(x=0; x<cardsNumber; x++){
+ 
+let imagex = chosenImages[x];
+let cardx = chosenCards[x];
+let baseCard = document.getElementById(cardx);
+console.log(cardx);
+baseCard.setAttribute('src', imagex);
+
+let baseCardParent = baseCard.parentElement.parentElement.parentElement;
+console.log(baseCardParent);
+baseCardParent.classList.remove("hidden");
 }
 
-function preGame(){
 
-}
-function hideCards(){
-    firstCard.classList.add("flip")
-    firstCard.classList.remove("fliped")
-    secondCard.classList.add("flip")
-    secondCard.classList.remove("fliped")
-}
+//função para revelar a carta clicada
 let enable = true;
-
-
 function showCard(clicked){
     if (enable === true){
     card = clicked;
@@ -35,13 +63,6 @@ function showCard(clicked){
     card.classList.add("fliped");
     console.log(enable);
 } else{}
-}
-
-function hideCards(){
-    firstCard.classList.add("flip");
-    firstCard.classList.remove("fliped");
-    secondCard.classList.add("flip");
-    secondCard.classList.remove("fliped");
 }
 
 
@@ -76,10 +97,10 @@ if(paridade === 0){
         function liberaClick(){
             enable = true;
         }
-        setTimeout(hideCards, 1000);
+        setTimeout(resetCards, 1000);
         setTimeout(liberaClick, 1000);
         
-        function hideCards(){
+        function resetCards(){
             console.log(firstCard);
             firstCard.classList.add("flip");
             firstCard.classList.remove("fliped");
